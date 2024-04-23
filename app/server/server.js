cc55a91d82829861f383147af1eb66671547a53c
RBray133 <123953365+RBray133@users.noreply.github.com>
2024-04-23 21:43:13 +0100

    Port to ejs

--- app/server/server.js
+++ app/server/server.js
@@ -1,19 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-app.set('views', path.join('client/', 'views'));
+const path = require("path");
+app.set("views", path.join("client/", "views"));
 app.set("view engine", "ejs");
 app.use(express.static("client/public"));
 
+const userRouter = require("./routes/users");
+const registerRouter = require("./routes/register");
 
+app.use("/users", userRouter);
+app.use("/register", registerRouter);
 
+const posts = [
+  { id: 1, title: "foo" },
+  { id: 2, title: "bah" },
+];
 
-const userRouter = require('./routes/users');
-const registerRouter = require('./routes/register');
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
 
-app.use('/users', userRouter);
-app.use('/register', registerRouter);
+app.get("/authed", (req, res) => {
+  res.render("logged_in_index", { posts });
+});
 
-app.listen(5000, () => { console.log("Server is running on port 5000") });
+app.get("/authed/posts/:id", (req, res) => {
+  const post_id = req.params.id;
+  const post = posts.find((post) => post.id == post_id);
 
-
+  if (post) {
+    res.render(`post${post.id}`, { post });
+  } else {
+    res.status(404);
+  }
+});
