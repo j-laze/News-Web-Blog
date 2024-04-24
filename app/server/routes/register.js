bec21bd5a6947c79684e36a32d891306a4aee4c4
j-laze <107155977+j-laze@users.noreply.github.com>
2024-04-24 21:56:45 +0100

    - modified routes and moving database over to web app

--- app/server/routes/register.js
+++ app/server/routes/register.js
@@ -1,8 +1,35 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const { User } = require('../../database/models'); // Assuming User model is exported from models/index.js
 const router = express.Router();
+
+router.use(express.json()); // for parsing application/json
+router.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 
 router.get('/', (req, res) => {
     res.render("register", { title: "Register Page" });
+});
+
+router.post('/', async (req, res) => {
+    const { username, email, password } = req.body;
+    console.log(username, email, password);
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            username,
+            email,
+            passwordHash: hashedPassword,
+        });
+        console.log('User created:', user.toJSON());
+
+        // Redirect to login page or wherever you want
+        res.redirect('/login');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error registering new user');
+    }
 });
 
 module.exports = router;
--- app/server/server.js
+++ app/server/server.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+//const passport = require('passport');
+//const LocalStrategy = require('passport-local').Strategy;
+//const bodyParser = require('body-parser');
+//const bcrypt = require('bcrypt');
+
 app.set('views', path.join('client/', 'views'));
 app.set("view engine", "ejs");
 app.use(express.static("client/public"));
 
 
+app.get('/', (req, res) => {
+    res.render("index", { title: "Home Page" });
 
-
-const userRouter = require('./routes/users');
+});
+const userRouter = require('./routes/user');
 const registerRouter = require('./routes/register');
+//const postRouter = require('./routes/post');
+//const loginRouter = require('./routes/login')(passport);
 
-app.use('/users', userRouter);
+
+app.use('/user', userRouter);
+//app.use('/post', postRouter);
+//app.use('/login', loginRouter);
 app.use('/register', registerRouter);
+
 
 app.listen(5000, () => { console.log("Server is running on port 5000") });
 
