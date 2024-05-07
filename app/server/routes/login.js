6e6a34dd6179b529c805769c2dbfc982dd6e5b7d
RBray133 <123953365+RBray133@users.noreply.github.com>
2024-05-07 21:20:29 +0100

    migrating form handiling to backend

--- app/server/routes/login.js
+++ app/server/routes/login.js
@@ -1,21 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
 // const passport = require('passport');
 // const localStrategy = require('passport-local').Strategy;
 
+router.use(bodyParser.urlencoded( { extended: false } ));
+router.use(express.json());
+
+function isValidEmail(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+function containsXSS(text) {
+    return text.match(/(<[^>]*>)(.*?)([^>]*>)/);
+}
 
 router.get('/', (req, res) => {
     res.render("login", { title: "Login Page" });
 });
 
-
 router.post('/', async (req, res) => {
-    const {username, password} = req.body;
+    const {email, password} = req.body;
 
+    console.log(`Email: ${email}`);
+    console.log(`Password: ${password}`);
 
+    //* Check for valid email address pattern
+    if (!isValidEmail(email)) {
+        
+        return res.redirect("/login");
+    }
+
+    //* Check that the password contain html tags
+    if (containsXSS(email) || containsXSS(password)) {
+        //console.log(containsXSS(email.value))
+        
+        return res.redirect("/login");
+    }
+
+    return res.redirect("/authed");
 });
-
-
 
 
 
