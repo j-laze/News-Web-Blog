53297a34fc64713062c1e7a990dfd8dfcbf3dc7f
RBray133 <123953365+RBray133@users.noreply.github.com>
2024-05-14 18:06:49 +0100

    gets posts from db

--- app/server/models/user.js
+++ app/server/models/user.js
@@ -1,87 +1,101 @@
-const {client: database}  = require('../database/initalizeDB.js');
-const jwt = require('jsonwebtoken');
-const {v4: uuidv4} = require("uuid");
-const bcrypt = require('bcrypt');
+const { client: database } = require("../database/initalizeDB.js");
+const jwt = require("jsonwebtoken");
+const { v4: uuidv4 } = require("uuid");
+const bcrypt = require("bcrypt");
 
-const jwt_secret = "test"
-
-
-
+const jwt_secret = "test";
 
 class User {
+  static async hash(item_to_hash) {
+    return await bcrypt.hash(item_to_hash, 10);
+  }
 
-    static async hash (item_to_hash){
-        return await bcrypt.hash(item_to_hash, 10);
+  static async getAllPosts() {
+    const query = `
+            SELECT * FROM posts
+            ORDER BY created_at
+        `;
+    try {
+      const result = await database.query(query);
+      //console.log(result.rows);
+
+      return result.rows;
+    } catch (error) {
+      console.error(error);
     }
+  }
 
+  static async getUserPosts(user_id) {
+    const query = `
+            SELECT * FROM posts
+            WHERE user_id = $1
+            ORDER BY created_at
+    `;
+    try {
+      const result = await database.query(query, [user_id]);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-    static async create({username, password, email }) {
-        const user_id = await uuidv4();
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const query = `
+  static async create({ username, password, email }) {
+    const user_id = await uuidv4();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const query = `
             INSERT INTO users (user_id, username, password, email)
             VALUES ($1, $2, $3, $4)`;
 
-        const values = [user_id, username, hashedPassword, email];
+    const values = [user_id, username, hashedPassword, email];
 
-        try {
-            await database.query(query, values);
-
-        } catch (error) {
-            console.error(error);
-            throw new Error('Error creating user');
-        }
+    try {
+      await database.query(query, values);
+    } catch (error) {
+      console.error(error);
+      throw new Error("Error creating user");
     }
+  }
 
-
-
-    static async loginCheck(users_email, password, res, req) {
-        console.log(users_email, password)
-        const query = `
+  static async loginCheck(users_email, password, res, req) {
+    console.log(users_email, password);
+    const query = `
         SELECT * FROM users
         WHERE email = $1`;
-        try {
-            const result = await database.query(query, [users_email]);
-            const user = result.rows[0];
-            if (!user) {
-                throw new Error('User not found');
-            }
-            const passwordMatch = await bcrypt.compare(password, user.password);
-            if (!passwordMatch) {
-                throw new Error('Password incorrect');
-            }
-            const token = await User.createUsertoken(user.user_id, req)
-            await User.sendCookie(token, res)
-            return user;
-
-        } catch (error) {
-            console.error(error);
-            throw new Error('Error logging in');
-        }
-
-         let cookie = req.cookies;
-        console.log(cookie)
-    }
-    static async sendCookie(token, res) {
-        res.cookie('token', token, {
-            httpOnly: true,
-            sameSite: 'none',
-            secure: false,
-            maxAge: 1000 * 60 * 60,
-            path: '/'
-        });
+    try {
+      const result = await database.query(query, [users_email]);
+      const user = result.rows[0];
+      if (!user) {
+        throw new Error("User not found");
+      }
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) {
+        throw new Error("Password incorrect");
+      }
+      const token = await User.createUsertoken(user.user_id, req);
+      await User.sendCookie(token, res);
+      return user;
+    } catch (error) {
+      console.error(error);
+      throw new Error("Error logging in");
     }
 
+    let cookie = req.cookies;
+    console.log(cookie);
+  }
+  static async sendCookie(token, res) {
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "none",
+      secure: false,
+      maxAge: 1000 * 60 * 60,
+      path: "/",
+    });
+  }
 
-
-   static async createUsertoken(user_id, req) {
-        let ipaddress = req.connection.remoteAddress;
-        let payload = {user_id, ipaddress}
-       return jwt.sign(payload, jwt_secret, {expiresIn: '1h'})
-
-   }
-
-
+  static async createUsertoken(user_id, req) {
+    let ipaddress = req.connection.remoteAddress;
+    let payload = { user_id, ipaddress };
+    return jwt.sign(payload, jwt_secret, { expiresIn: "1h" });
+  }
 }
 
 module.exports = { User };
--- app/server/routes/auth.js
+++ app/server/routes/auth.js
@@ -1,55 +1,57 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cookieParser = require("cookie-parser");
 const router = express.Router();
 router.use(cookieParser());
-const { User } = require('../models/user.js'); // user models from db
+const { User } = require("../models/user.js"); // user models from db
 
-router.get('/', isauthed,  (req, res) => {
-    res.render('logged_in_index', { title: 'Logged In Index' });
-
+router.get("/", isauthed, async (req, res) => {
+  try {
+    const posts = await User.getAllPosts();
+    console.log("posts here", posts);
+    res.render("logged_in_index", { title: "Logged In Index", posts });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-
-const jwt_secret = 'test';
-const { client: database } = require('../database/initalizeDB.js');
+const jwt_secret = "test";
+const { client: database } = require("../database/initalizeDB.js");
 const jwt = require("jsonwebtoken");
 
-
-
 async function isauthed(req, res, next) {
-    try {
-        // Verify the token and extract the user ID
-        const decodedToken = jwt.verify(req.cookies.token, jwt_secret);
-        req.user_id = decodedToken.user_id;
-        const decoded_user_id = req.user_id
-        console.log("userid", decoded_user_id)
-        const query = 'SELECT * FROM users WHERE user_id = $1';
-        const values = [decoded_user_id];
-        const result = await database.query(query, values);
-        if (result.rows.length > 0) {
-            // User exists, check ipaddress matches
-            const current_user_ip = req.connection.remoteAddress;
-            if(decodedToken.ipaddress !== current_user_ip){
-                console.log("IP Address does not match");
-                console.log(decodedToken.ipaddress, current_user_ip);
-                return res.status(401).render('login', {
-                    message: 'Invalid token'
-                });
-            }
-            // User is authenticated, proceed to the next middleware function or route handler
-            next();
-        } else {
-            // User does not exist, return an error
-            return res.status(401).render('login', {
-                message: 'Invalid token'
-            });
-        }
-    } catch (err) {
-        // Token is not valid or user does not exist
-        console.log(err.message);
-        res.status(401).render('login', {
-            message: 'Token expired or tampered'
+  try {
+    // Verify the token and extract the user ID
+    const decodedToken = jwt.verify(req.cookies.token, jwt_secret);
+    req.user_id = decodedToken.user_id;
+    const decoded_user_id = req.user_id;
+    console.log("userid", decoded_user_id);
+    const query = "SELECT * FROM users WHERE user_id = $1";
+    const values = [decoded_user_id];
+    const result = await database.query(query, values);
+    if (result.rows.length > 0) {
+      // User exists, check ipaddress matches
+      const current_user_ip = req.connection.remoteAddress;
+      if (decodedToken.ipaddress !== current_user_ip) {
+        console.log("IP Address does not match");
+        console.log(decodedToken.ipaddress, current_user_ip);
+        return res.status(401).render("login", {
+          message: "Invalid token",
         });
+      }
+      // User is authenticated, proceed to the next middleware function or route handler
+      next();
+    } else {
+      // User does not exist, return an error
+      return res.status(401).render("login", {
+        message: "Invalid token",
+      });
     }
+  } catch (err) {
+    // Token is not valid or user does not exist
+    console.log(err.message);
+    res.status(401).render("login", {
+      message: "Token expired or tampered",
+    });
+  }
 }
 module.exports = router;
