4e5bad2767c4f030cc4362c758c3f1b910b687f5
j-laze <107155977+j-laze@users.noreply.github.com>
2024-05-14 14:53:28 +0100

    - modified user posts table by removing unnecessary values
    - fixed foreign key bug in table
    
    - fixed typo  so initializeDB() is now imported correctly
    
    - changed db password to default, so each user to run the project can insert their password there

--- app/server/database/initalizeDB.js
+++ app/server/database/initalizeDB.js
@@ -1,12 +1,13 @@
-const { Client } = require("pg");
+const { Client } = require('pg');
 
 const client = new Client({
-  user: "postgres",
-  host: "localhost",
-  database: "news_blog",
-  password: "postgres123",
-  port: 5432,
+    user: 'postgres',
+    host: 'localhost',
+    database: 'news_blog',
+    password: 'YOUR_PASSWORD_HERE',
+    port: 5432,
 });
+
 
 const createUsersTable = `
     CREATE TABLE IF NOT EXISTS users (
@@ -17,15 +18,34 @@ const createUsersTable = `
     );
 `;
 
-async function initalizeDB() {
-  await client.connect();
-  await client.query(createUsersTable, (err, res) => {
-    if (err) {
-      console.error("Error executing query", err.stack);
-    } else {
-      console.log("Table created successfully");
+
+
+
+const createPostsTable = `
+    CREATE TABLE IF NOT EXISTS posts (
+        post_id SERIAL PRIMARY KEY,
+        user_id VARCHAR(36),
+        title VARCHAR(255) NOT NULL,
+        content TEXT NOT NULL,
+        created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+    );
+`;
+
+
+async function initializeDB() {
+    try {
+        await client.connect();
+        await client.query(createUsersTable);
+        console.log('Users table created successfully');
+        await client.query(createPostsTable);
+        console.log('Posts table created successfully');
+    } catch (err) {
+        console.error('Error executing query', err.stack);
     }
-  });
 }
 
-module.exports = { client, initalizeDB };
+
+
+
+module.exports = { client, initializeDB}
